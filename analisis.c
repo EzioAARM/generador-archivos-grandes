ae0a96f0de7bfcb413a0f87e3c997246be513bc8
Axel Rodriguez <alejandrom9712@gmail.com>
2020-03-08 21:13:40 -0600

    cambios varios, el principal es dejar de usar fseek y usar fseeko

--- analisis.c
+++ analisis.c
@@ -9,13 +9,13 @@
 FILE *archivoGrande;
 pthread_mutex_t lock;
 pthread_mutex_t lockVar;
-int cantThreads = 3;
-pthread_t threads[3];
-bool terminados[3];
+int cantThreads = 4;
+pthread_t threads[4];
+bool terminados[4];
 double bytesToRead;
 double startLine = 891;
 double startReading = 0;
-int conteo[100];
+double conteo[255];
 
 struct args {
     double bytesStart;
@@ -29,13 +29,15 @@ void *readingThread(void *input) {
     int threadPos = ((struct args*)input)->pos;
     char letras[400];
     char c;
-    int conteoInterno[100];
+    double conteoInterno[255];
     int lastRead = 400;
-    for (int i = bytePos; i <= bytePosEnd; i += 400) {
+    for (double i = bytePos; i <= bytePosEnd; i += 400) {
         lastRead = ((i + 400) > bytePosEnd) ? (bytePosEnd - i) : 400;
         pthread_mutex_lock(&lock);
-            fseek(archivoGrande, i, SEEK_SET);
-            fread(&letras, 1, lastRead, archivoGrande);
+            if (fseeko(archivoGrande, i, SEEK_SET) != 0) {
+                printf("Hubo un error en el thread %i\n", threadPos);
+            } else 
+                fread(&letras, 1, lastRead, archivoGrande);
         pthread_mutex_unlock(&lock);
         // imprime, aqui debe ir la cuenta
         for (int i = 0; i <= lastRead; i++) {
@@ -45,7 +47,7 @@ void *readingThread(void *input) {
         //printf("thread %i: %i\n", ((struct args*)input)->pos, i);
     }
     pthread_mutex_lock(&lockVar);
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 0; i <= 255; i++) {
             conteo[i] += conteoInterno[i];
         }
         terminados[threadPos] = true;
@@ -54,26 +56,27 @@ void *readingThread(void *input) {
             //printf("%d\n", terminados[i]);
             if (!terminados[i]) finished = false;
         }
+        printf("\nEl thread %i termino \n", threadPos);
         if (finished) {
             fclose(archivoGrande);
             printf("Terminaron todos los threads\n");
         }
     pthread_mutex_unlock(&lockVar);
-    //printf("\nEl thread %i termino \n", threadPos);
 }
 
 int main() {
     for (int i = 0; i < cantThreads; i++) {
         terminados[i] = false;
     }
+    terminados[cantThreads] = true;
     printf("Ingrese la ubicacion de su archivo\n");
     char ubicacion[1000];
     scanf("%s", ubicacion);
-    system("clear");
+    //system("clear");
     archivoGrande = fopen(ubicacion, "r");
-    fseek(archivoGrande, 0L, SEEK_END);
+    fseeko(archivoGrande, 0LL, SEEK_END);
     // Obtiene el peso del archivo
-    double fileSize = ftell(archivoGrande);
+    double fileSize = ftello(archivoGrande);
     if ((fileSize / 1e+9) < 1) {
         printf("El archivo pesa: %lf mb (%lf bytes)\n", fileSize / 1e+6, fileSize);
     } else {
@@ -85,7 +88,7 @@ int main() {
     struct args *myStruc;
     double start = startLine;
     startReading = bytesToRead;
-    double bytesThread = ceil(bytesToRead / cantThreads);
+    double bytesThread = ceil(bytesToRead / (cantThreads));
     double end = start + bytesThread;
     //printf("%lf\n", bytesThread);
     while (i < cantThreads) {
@@ -94,7 +97,7 @@ int main() {
         myStruc->byteEnd = end;
         myStruc->pos = i;
         pthread_create(&threads[i], NULL, readingThread, myStruc);
-        //printf("El thread %i leera %lf bytes hasta %lf\n", i, start, end);
+        printf("El thread %i leera %lf bytes hasta %lf\n", i, start, end);
         start = end + 1;
         end = start + bytesThread;
         if (end >= bytesToRead) {
@@ -102,10 +105,13 @@ int main() {
         }
         i++;
     }
+    for (int j = 0; j < cantThreads; j++) {
+        pthread_join(threads[j], NULL);
+    }
     bool terminoThreads = false;
     char cosa[100] = "";
     while (!terminoThreads) {
-        system("clear");
+        //system("clear");
         terminoThreads = true;
         for (int i = 0; i < cantThreads; i++) {
             if (!terminados[i]) terminoThreads = false;
@@ -114,7 +120,7 @@ int main() {
         if (!terminoThreads) usleep(30e6);
     }
     for (int i = 65; i <= 90; i++) {
-        printf("%c -> %i\n", (char) i, conteo[i]);
+        printf("%c -> %lf\n", (char) i, conteo[i]);
     }
     return 0;
 }
